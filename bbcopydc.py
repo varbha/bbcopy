4cb3b2d1563e6f04a23c3f211bc51fb4ad8927d5
varbha <varunb95@gmail.com>
2017-09-19 08:15:00 +0530

    download for DC added

--- bbcopydc.py
+++ bbcopydc.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
-import requests, bs4
-import os
+import requests, bs4, os, shutil
+# try:
+#     path = "C:\\BBCopy"
+#     shutil.rmtree(path)
+# # ^ really really inefficient, but this removes the destination directory from the host OS.
+# # had to do this because the program has no way of knowing the files already present in the destination (as of now),
+# # so it downloads all the files on BB. ^ statement removes possibility of duplicates
+# except Exception as e:
+#     print("Most likely - you dont have a directory as C:\BBCopy. Make that manually, code will run as soon as you do that")
+#     print(e)
+
+# TODO
+# find a way to compare downloaded files and saved files. (CSV, with file names ?)
 
 fp = webdriver.FirefoxProfile()
 
 fp.set_preference("browser.download.folderList",2)
 fp.set_preference("browser.download.manager.showWhenStarting",False)
-fp.set_preference("browser.download.dir", 'C:\\BBCopy\\DWM') # directory to save the file
+fp.set_preference("browser.download.dir", 'C:\\BBCopy\\DC') # directory to save the file
 # change the file MIME type here
 fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.openxmlformats-officedocument.wordprocessingml.document") # MIME type of .docx files
 
@@ -27,12 +38,13 @@ try:
     print("Navigating to courses..")
     c.click()
     # change subject name here
-    p = d.find_element_by_link_text('NMMPSMU_1718_CL_FBTE0CS_07_0B_03: MPS (M) B. Tech (Comp) 0B S07 () - Data Warehousing and Mining')
+    p = d.find_element_by_link_text('NMMPSMU_1718_CL_FBTE0CS_07_0B_01: MPS (M) B. Tech (Comp) 0B S07 () - Distributed Computing')
 
     p.click()
     print("Chosen subject selected..")
-    c=d.find_element_by_id('paletteItem:_296487_1')
+    c=d.find_element_by_id('paletteItem:_296471_1')
     c.click()
+    d.implicitly_wait(30)
     print("Navigated to Course Tools")
     j = d.find_element_by_id('anonymous_element_20')
     j.click()
@@ -54,9 +66,9 @@ try:
         except Exception as e:
             print(e)
         try:
+            d.implicitly_wait(20)
             file = d.find_element_by_xpath('html/body/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div[1]/div/div[2]/ul/li[2]/a')
             file.click()
-            print("File Found..")
             print("Check destination folder for file download")
         except Exception as e:
             print("No attachement found for index= ", starting_index)
